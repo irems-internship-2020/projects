58925841982c504ca0654955f887b5a80a7a3ffb
Norbi <jambornorbert1@gmail.com>
2020-08-11 12:53:50 +0300

    Add address book application

---
+++ Norbert Jambor/Address Book/src/address_book/Perspective.java
@@ -0,0 +1,11 @@
+package address_book;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class Perspective implements IPerspectiveFactory {
+
+	@Override	
+	public void createInitialLayout(IPageLayout layout) {
+	}
+}
