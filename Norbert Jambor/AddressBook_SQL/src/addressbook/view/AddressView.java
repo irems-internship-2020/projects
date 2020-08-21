066e57405e49bf2971243a7efc0806af0bd7e37b
Norbi <jambornorbert1@gmail.com>
2020-08-21 15:04:19 +0300

    Add AddressBook_SQL project V2

---
+++ Norbert Jambor/AddressBook_SQL/src/addressbook/view/AddressView.java
@@ -0,0 +1,97 @@
+package addressbook.view;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.ui.part.ViewPart;
+
+import addressbook.enumLabels.AddressColumnLabels;
+import addressbook.model.Contact;
+
+import org.eclipse.jface.viewers.TableViewerColumn;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.swt.widgets.Text;
+
+public class AddressView extends ViewPart {
+	public static final String ID = "addressbook.editor.create";
+
+	private TableViewer viewer;
+
+	private AddressColumnLabels titleProvider;
+
+	List<TableViewerColumn> columnList = new ArrayList<TableViewerColumn>();
+
+	public void createPartControl(Composite parent) {
+		GridLayout layout = new GridLayout(2, false);
+		parent.setLayout(layout);
+		Label searchLabel = new Label(parent, SWT.NONE);
+		searchLabel.setText("Search: ");
+		final Text searchText = new Text(parent, SWT.BORDER | SWT.SEARCH);
+		searchText.setLayoutData(new GridData(GridData.GRAB_HORIZONTAL | GridData.HORIZONTAL_ALIGN_FILL));
+		createViewer(parent);
+	}
+
+	private void createViewer(Composite parent) {
+		viewer = new TableViewer(parent, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL | SWT.FULL_SELECTION | SWT.BORDER);
+		createColumns(parent, viewer);
+		final Table table = viewer.getTable();
+		table.setHeaderVisible(true);
+		table.setLinesVisible(true);
+		
+		viewer.setContentProvider(new ArrayContentProvider());
+		getSite().setSelectionProvider(viewer);
+		
+		GridData gridData = new GridData();
+		gridData.verticalAlignment = GridData.FILL;
+		gridData.horizontalSpan = 2;
+		gridData.grabExcessHorizontalSpace = true;
+		gridData.grabExcessVerticalSpace = true;
+		gridData.horizontalAlignment = GridData.FILL;
+		viewer.getControl().setLayoutData(gridData);
+	}
+	
+	public void setInput(Contact firstElement) {
+		viewer.setInput(Arrays.asList(firstElement));
+		viewer.refresh();
+	}
+
+	public TableViewer getViewer() {
+		return viewer;
+	}
+
+	@SuppressWarnings("static-access")
+	private void createColumns(final Composite parent, final TableViewer tableViewer) {
+		int counter = 0;
+
+		for (AddressColumnLabels title : titleProvider.values()) {
+			TableViewerColumn col = createTableViewerColumn(title.getText(), 100, counter);
+			columnList.add(col);
+			counter++;
+		}
+
+		tableViewer.setLabelProvider(new AddressLabelProvider(columnList));
+	}
+
+	private TableViewerColumn createTableViewerColumn(String title, int bound, final int colNumber) {
+		final TableViewerColumn viewerColumn = new TableViewerColumn(viewer, SWT.NONE);
+		final TableColumn column = viewerColumn.getColumn();
+		column.setText(title);
+		column.setWidth(bound);
+		column.setResizable(true);
+		column.setMoveable(true);
+		return viewerColumn;
+	}
+
+	public void setFocus() {
+		viewer.getControl().setFocus();
+	}
+}
